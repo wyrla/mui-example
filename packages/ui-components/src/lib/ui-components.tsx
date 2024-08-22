2d6a8fed4f462b6fc07bc49921f2105fb4494749
wyrla <111397148+wyrla@users.noreply.github.com>
2024-08-22 12:57:15 -0300

    chore: add ui lib

---
+++ packages/ui-components/src/lib/ui-components.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const StyledUiComponents = styled.div`
+  color: pink;
+`;
+
+export function UiComponents() {
+  return (
+    <StyledUiComponents>
+      <h1>Welcome to UiComponents!</h1>
+    </StyledUiComponents>
+  );
+}
+
+export default UiComponents;
