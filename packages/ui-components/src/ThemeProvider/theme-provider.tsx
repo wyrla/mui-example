0c8eb15bd991fe1ce86b6eb2cb7769537372fff6
wyrla <111397148+wyrla@users.noreply.github.com>
2024-08-22 15:54:41 -0300

    feat: add theme provider component

---
+++ packages/ui-components/src/ThemeProvider/theme-provider.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { createTheme, ThemeProvider as MTP } from '@mui/material/styles';
+import { Theme } from './theme';
+import { ThemeOptions, themes } from './predefined-themes';
+
+const DEFAULT_THEME = {
+  name: 'default',
+  palette: {
+    primary: {
+      '100': '#33648a',
+      '200': '#459fa1',
+    },
+    secondary: {
+      '100': '#93d2b7',
+      '200': '#f0e2a3',
+    },
+    success: {
+      '100': '#f7daab',
+    },
+  },
+};
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  theme?: Theme | ThemeOptions;
+}
+
+type IThemeContext = React.Context<Theme>;
+const ThemeContext: IThemeContext = React.createContext(DEFAULT_THEME);
+
+type UseTheme = () => Theme;
+export const useTheme: UseTheme = () => React.useContext(ThemeContext);
+
+const resolveMui = (theme: Theme) =>
+  createTheme({
+    palette: {
+      primary: {
+        main: theme.palette.primary[100],
+      },
+      secondary: {
+        main: theme.palette.secondary[100],
+      },
+      success: {
+        main: theme.palette.success[100],
+      },
+    },
+  });
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({
+  children,
+  theme = DEFAULT_THEME,
+}: ThemeProviderProps) => {
+  const t = typeof theme === 'string' ? themes[theme] : theme;
+  const compatibleTheme = resolveMui(t);
+  return (
+    <ThemeContext.Provider value={t}>
+      <MTP theme={compatibleTheme}>{children}</MTP>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/ui-components';
 export * from './Button';
 export * from './IconButton';
+export * from './ThemeProvider';
